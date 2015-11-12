335b23ade46aa65b2c1ef16615a30535d0095d9e
Milos Kovacevic <miloscpt@gmail.com>
2015-11-12 17:06:51 +0100

    Added state and some functions

---
+++ js/Note.js
@@ -0,0 +1,59 @@
+/**
+ * Created by Ana on 11/12/2015.
+ */
+
+
+var Note = React.createClass({
+    getInitialState: function(){
+        return {
+           editing: false
+        }
+    },
+    edit: function () {
+        this.setState({
+            editing: true
+        });
+    },
+    save: function(){
+        this.setState({
+            editing: false
+        });
+    },
+    remove: function () {
+        alert("remove note");
+    },
+    renderDisplay: function() {
+        return (
+            <div className="note">
+                <p>{this.props.children}</p>
+                <span>
+                <button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil" />
+                <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash" />
+                </span>
+            </div>
+        );
+    },
+    renderForm: function(){
+        return (
+            <div className="note">
+                <textarea defaultValue={this.props.children} className="form-control"></textarea>
+                <button onClick={this.save} className="btn btn-success btn-sm glyphicon-floppy-disk" />
+            </div>
+        );
+    },
+    render: function () {
+        if(this.state.editing)
+        {
+            return this.renderForm();
+        }
+        else
+        {
+            return this.renderDisplay();
+        }
+    }
+});
+
+React.render(
+    <Note>Mothafuckaaaaaaa!!!!!</Note>,
+    document.getElementById("react-container")
+);
