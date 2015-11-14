58c873cd3a17de5a14bd1805c8f76ad249a1dc8c
Milos <miloscpt@gmail.com>
2015-11-14 16:33:56 +0100

    Implemented Update and Remove sticker functionality

--- js/Note.js
+++ js/Note.js
@@ -6,20 +6,27 @@
 var Note = React.createClass({
     getInitialState: function(){
         return {
-           editing: false
+           editing: false,
+           milos: "djubre - prvi slucaj"
         }
     },
     edit: function () {
         this.setState({
-            editing: true
+            editing: true,
+            milos: "nije djubre - kliknuo na edit"
         });
     },
     save: function(){
+
+        this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);
+
         this.setState({
-            editing: false
+            editing: false,
+            milos: "djubre - kliknuo na save"
         });
     },
     remove: function () {
+        this.props.onRemove(this.props.index);
         alert("remove note");
     },
     renderDisplay: function() {
@@ -36,12 +43,15 @@ var Note = React.createClass({
     renderForm: function(){
         return (
             <div className="note">
-                <textarea defaultValue={this.props.children} className="form-control"></textarea>
+                <textarea ref="newText" defaultValue={this.props.children} className="form-control"></textarea>
                 <button onClick={this.save} className="btn btn-success btn-sm glyphicon-floppy-disk" />
             </div>
         );
     },
     render: function () {
+        //debug purposes...
+        console.log(this.state.milos);
+
         if(this.state.editing)
         {
             return this.renderForm();
@@ -53,7 +63,75 @@ var Note = React.createClass({
     }
 });
 
+//parent component for our Notes. Notes will be displayed on the Board
+var Board = React.createClass({
+
+    propTypes: {
+        count: function(props, propName){
+            if (typeof props[propName] !== "number"){
+                return new Error("Count property must be a number!");
+            }
+            if(props[propName] > 100){
+                return new Error("Creating " + props[propName] + " notes is ridiculous!");
+            }
+        }
+    },
+
+    getInitialState: function(){
+      return {
+          notes: [
+              'Call Bill',
+              'Email Lisa',
+              'Make dentist appt',
+              'Send Proposal'
+          ]
+      };
+    },
+
+    update: function(newText, i){
+
+        var arr = this.state.notes;
+        arr[i] = newText;
+
+        this.setState({
+            notes: arr
+        });
+    },
+
+    remove: function(i){
+
+        var arr = this.state.notes;
+        arr.splice(i, 1);
+
+        this.setState({
+           notes: arr
+        });
+    },
+
+    eachNote: function(note, i){
+        return (
+            <Note key={i}
+                index={i}
+                onChange={this.update}
+                onRemove={this.remove}
+                >{note}</Note>
+        );
+    },
+
+    render: function(){
+
+        return (
+            <div className="board">
+                {this.state.notes.map(this.eachNote)}
+                <p ref="mikac"></p>
+            </div>
+        );
+
+
+    }
+});
+
 React.render(
-    <Note>Mothafuckaaaaaaa!!!!!</Note>,
+    <Board count={10} />,
     document.getElementById("react-container")
 );
